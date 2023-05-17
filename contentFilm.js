2050b6421fd3564e5479dc1d5dc53ccc563754a6
xuandanghoc <newwors0123@gmail.com>
2023-05-17 16:16:48 +0700

    cinema14

---
+++ contentFilm.js
@@ -0,0 +1,16 @@
+let contentFilms = [
+    {
+        source: 'Top phim',
+        time: '9 phút đọc',
+        titleSub: 'AI là đề tài được bàn tán phổ biến trong thời đại công nghệ hiện nay. Tuy nhiên, ít ai biết được đề tài trí tuệ nhân tạo đã xây dựng và tiên đoán từ rất lâu qua màn ảnh.  Cùng ví Momo điểm danh những phim AI đặc sắc qua dòng lịch sử nhé!',
+        listFilms: [
+            {
+                title: '1. Thủ phủ - Metropolis (1927)',
+                paragragh1: 'Nhìn qua năm ra đời của phim là bạn cũng bất ngờ rồi đúng không? Ra mắt trong Thế chiến thứ hai, phim câm của Đức được xem là tác phẩm mở đầu cho thể loại khoa học giả tưởng. Phim lấy bối cảnh ở Metropolis - một thành phố do Joh Fredesen sáng lập, với tầng trên là các toà nhà cao tầng, khu giải trí sầm uất và tầng ngầm là khu công nghiệp bóc lột sức lao động công nhân đến tột cùng. Cậu con trai Freder vốn dĩ ăn chơi đột ngột trúng tiếng sét ái tình với Maria, người thuyết giáo đứng về người lao động và con cái họ. ',
+                paragragh2: 'Vì người yêu, cậu công tử quyết định tham quan nhà máy dưới lòng đất và kinh hoàng trước cảnh công nhân bị kiệt sức. Freder quyết tâm trở thành người xoá nhoà khoảng cách giữa giai cấp tư sản và người công nhân. Ý định này khiến ngài Fredersen vô cùng tức giận. Ông quyết cùng nhà khoa học điên rồ Rotwang tạo ra con robot mô phỏng y hệt Maria nhằm khôi phục trật tự cũ. Bộ phim phản ánh sự xấu xí của chủ nghĩa tư bản cũng như sự vô nhân tính của đám đông trước cám dỗ. Các tình tiết, biểu cảm của từng nhân vật và hình ảnh xuất sắc khiến phim lôi cuốn dù thời điểm ấy công nghệ dựng phim còn rất thô sơ và không có tiếng nhân vật. ',
+                image: 'https://static.mservice.io/blogscontents/momo-upload-api-220316120941-637830293813841291.jpg',
+                subImage: 'Nhân vật robot đã trở thành biểu tượng lịch sử trong ngành và được làm lại trưng bày trong Viện Khoa học Carnegie ở Mỹ'
+            }
+        ]
+    }
+]
